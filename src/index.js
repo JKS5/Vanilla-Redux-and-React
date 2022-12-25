0bf5ad4af9b5df97ebc1c4ba959717c3cd01870a
JKS5 <jokekhanson@gmail.com>
2022-12-25 21:23:48 +0900

    [VanillaJS+Redux] Step1:you can see how it works

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-const updateText = () => {
-  number.innerText = count;
+const reducer = (initalState = 0, action) => {
+  console.log(initalState, action);
+  if (action.type === "ADD") {
+    return initalState + 1;
+  } else if (action.type === "Minus") {
+    return initalState - 1;
+  }
+  console.log(initalState);
+  return initalState;
 };
 
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  store.dispatch({ type: "ADD" });
 };
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+const store = createStore(reducer);
+
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "ADD" });
+console.log(store.getState());
 
 add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// minus.addEventListener("click", handleMinus);
